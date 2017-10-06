4e5463346e6c7b1c9f7971ec4e6a21462d6d40dc
MAinulYaqin <ainul.y9b@gmail.com>
2017-10-06 14:07:46 +0700

    commit all files

--- demo/routes/index.js
+++ demo/routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Express'
+    });
+});
+
+router.get('/demo1', function (req, res, next) {
+    res.render('demo1', {
+        message: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+        user: {
+            name: 'Ainul',
+            email: 'ainul.y9b@gmail.com',
+            website: 'http://www.belajarnge.web.id'
+        }
+    });
 });
 
 module.exports = router;
